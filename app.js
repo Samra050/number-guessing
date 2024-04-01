f4ea12f36cb94fafeb34cd208c9a86042c834b6d
Syeda Samra <159478028+Samra050@users.noreply.github.com>
2024-04-01 13:31:08 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userguessednumber",
+        type: "number",
+        message: "please guess a number between 1-10"
+    },
+]);
+if (answer.userguessednumber === randomnumber) {
+    console.log("congtratulations you guessed the right number.");
+}
+else {
+    console.log("you guessed the wrong number");
+}
